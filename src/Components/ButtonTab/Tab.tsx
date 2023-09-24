738ce2aac2e181a064521e41fc26f43043e8baf2
ArleysGatica <gatica.arleys16@gmail.com>
2023-09-24 12:40:26 -0600

    Tab custom

---
+++ src/Components/ButtonTab/Tab.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AppDrawer from '../Drawer/Drawer';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { TouchableOpacity } from 'react-native';
+import { Welcome } from '../Welcome/Welcome';
+
+type HomeScreenProps = {
+    navigation: NativeStackNavigationProp<RootStackParamList, 'Welcome'>;
+};
+
+type RootStackParamList = {
+    Welcome: undefined;
+};
+
+const Tab = createBottomTabNavigator();
+
+const CustomBottomTabNavigator = ({ navigation }: HomeScreenProps) => {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarActiveTintColor: 'red',
+            }}
+            safeAreaInsets={{ bottom: 0 }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={AppDrawer}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <TouchableOpacity
+                            onPress={() => {
+                                navigation.navigate('Welcome');
+                            }}
+                        >
+                            <Icon name="home" size={30} color={color} />
+                        </TouchableOpacity>
+                    ),
+                    headerShown: false,
+                }}
+            />
+        </Tab.Navigator>
+    );
+};
+
+export default CustomBottomTabNavigator;
