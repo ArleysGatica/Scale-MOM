1e5cf77bb4b31660757fc23c93a80ef6b9c6a8a0
arleygatica <gatica.arleys16@gmail.com>
2023-10-22 23:53:37 -0600

    New login

---
+++ src/UI/Components/Login/Login.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../../types/types';
+import { Text, View, TextInput, Image, Button, TouchableOpacity, SafeAreaView, StyleSheet, TouchableHighlight, Alert, ActivityIndicator, Pressable } from 'react-native';
+import style from './Style.Login';
+
+type LoginFormProps = {
+    navigation?: NativeStackNavigationProp<RootStackParamList, 'DoctorCreationForm' | 'Login' | 'CustomBottomTabNavigator'>;
+};
+
+export function Login({ navigation }: LoginFormProps) {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    // const navigation = useNavigation();
+
+    const handleLogin = () => {
+        if (username === 'admin' && password === 'admin') {
+            navigation?.navigate('DoctorCreationForm');
+            Alert.alert('Entrar como admin');
+        } else if (username === 'doctor' && password === 'doctor') {
+            navigation?.navigate('CustomBottomTabNavigator');
+            Alert.alert('Entrar como doctor');
+        } else {
+            Alert.alert('Credenciales incorrectas');
+        }
+    };
+
+    return (
+        <SafeAreaView style={style.containerLogin}>
+            <View style={style.Container}>
+                <Image source={require('../../../Assets/LogoappLogo.png')}
+                    style={style.Logo} />
+                <Text style={style.txtSegund}>Login</Text>
+            </View>
+            <View style={{ width: '100%', alignItems: 'center' }}>
+
+                <View style={style.passwordContainer}>
+                    <TextInput
+                        style={style.passwordInput}
+                        placeholderTextColor="gray"
+                        placeholder="Name"
+                        onChangeText={(text) => setUsername(text)}
+                    />
+                </View>
+
+                <View style={style.passwordContainer} >
+                    <TextInput
+                        style={style.passwordInput}
+                        placeholderTextColor="gray"
+                        placeholder="username"
+                        onChangeText={(text) => setPassword(text)}
+                    />
+                </View>
+            </View>
+            <View style={{ width: '100%', alignItems: 'center', top:50 }}>
+                <TouchableHighlight style={style.buttonSing}>
+                    <Button
+                        color='rgb(18, 19, 48)'
+                        onPress={() => handleLogin()}
+                        title='Login'
+                        testID='login-button'
+                   />
+                </TouchableHighlight>
+            </View>
+        </SafeAreaView>
+    );
+
+}
+
+
+
+
