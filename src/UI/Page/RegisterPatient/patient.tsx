d16da09f1213ebc44fd0a138e7048e0054c5288b
arleygatica <gatica.arleys16@gmail.com>
2023-10-30 16:15:33 -0600

    Registar with ScrollView

--- src/UI/Page/RegisterPatient/patient.tsx
+++ src/UI/Page/RegisterPatient/patient.tsx
@@ -1,4 +1,4 @@
-import {  View, Image, TouchableOpacity, SafeAreaView, StyleSheet, TouchableHighlight, Alert, ActivityIndicator, Pressable } from 'react-native';
+import { View,  TouchableOpacity, SafeAreaView, StyleSheet, Alert,  Pressable, ScrollView } from 'react-native';
 
 import React, { useState, useEffect } from 'react';
 import { IPatient } from '../../../types/types';
@@ -20,7 +20,7 @@ const RegisterPatient = () => {
     ];
 
     const createPatientHandler = async () => {
-        const response = await fetchCreatePatient({...registerData, userType:0});
+        const response = await fetchCreatePatient({ ...registerData, userType: 0 });
         console.log(response);
     }
 
@@ -35,76 +35,98 @@ const RegisterPatient = () => {
 
     return (
         <>
-        
-          <Appbar.Header>
-                <Appbar.BackAction onPress={() => { }} />
-                <Text style={{ fontSize: 25 }}>
-                    Salir
-                </Text>
-            </Appbar.Header>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
+            <View style={styles.container}>
+                <Appbar.Header >
+                    <Appbar.BackAction onPress={() => { }} />
+                    <Text style={{ fontSize: 25 }}>Salir</Text>
+                </Appbar.Header>
+                <ScrollView contentContainerStyle={styles.scrollViewContent} keyboardShouldPersistTaps="handled">
+                    <Text
+                        style={{
+                            width: '100%',
+                            textAlign: 'center',
+                            fontSize: 45,
+                            fontWeight: 'bold',
+                            marginBottom: 20,
+                            color: 'black'
+                        }}
 
-                <Text
-                    style={{
-                        width: '100%',
-                        textAlign: 'center',
-                        fontSize: 45,
-                        fontWeight: 'bold',
-                        marginBottom: 20,
-                        color: 'black'
-                    }}
+                        variant="displayMedium">
+                        Registro de Pacientes
+                    </Text>
+                    {
+                        objData.map((item, index) => {
+                            return (
+                                <View key={index}
+                                    style={{
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        width: '100%',
+                                        height: 80,
+                                        justifyContent: 'center',
+                                        flexWrap: 'wrap',
+                                        gap: 10,
+                                        columnGap: 10,
+                                        rowGap: 10,
 
-                    variant="displayMedium">
-                    Registro de Pacientes
-                </Text>
-            {
-                objData.map((item, index) => {
-                    return (
-                        <View key={index}
-                            style={{
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                width: '100%',
-                                height: 80,
-                                justifyContent: 'center',
-                                flexWrap: 'wrap',
-                                gap: 10,
-                                columnGap: 10,
-                                rowGap: 10,
+                                    }}>
+                                    <Text style={{ width: 75, textAlign: 'center', fontSize: 15, }}>{item.label}</Text>
+                                    <TextInput
+                                        style={{
+                                            width: 200, height: 50,
+                                            marginBottom: 10,
+                                            backgroundColor: 'rgb(255, 255, 255)',
+                                            borderColor: 'rgba(0, 0, 0, 0.29)',
+                                            borderRadius: 4,
+                                            borderWidth: 1,
+                                            color: 'black',
+                                        }}
+                                        onChangeText={(text) => handleTextChange(item.name, text)}
+                                        value={registerData[item.name]?.toString()}
 
-                            }}>
-                            <Text style={{ width: 75, textAlign: 'center', fontSize:15, }}>{item.label}</Text>
-                            <TextInput
-                                style={{
-                                    width: 200, height: 50,
-                                    marginBottom: 10,
-                                    backgroundColor: 'rgb(255, 255, 255)',
-                                    borderColor: 'rgba(0, 0, 0, 0.29)',
-                                    borderRadius: 4,
-                                    borderWidth: 1,
-                                    color: 'black',
-                                }}
-                                onChangeText={(text) => handleTextChange(item.name, text)}
-                                value={registerData[item.name]?.toString()}
-
-                            />
-                        </View>
-                    )
-                })
-                }
-                
-            <Button
-                onPress={createPatientHandler}
-                    icon="account-check"
-                    mode="contained"
-                    style={{ width: 200, height: 50, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}
-                >
-                    Registrado
-                </Button>
+                                    />
+                                </View>
+                            )
+                        })
+                    }
+                    <View style={{ width: '100%', alignItems: 'center', top: 20 }}>
+                        <Button
+                            onPress={createPatientHandler}
+                            icon="account-check"
+                            mode="contained"
+                            style={{ width: 200, height: 50, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}
+                        >
+                            Registrado
+                        </Button>
+                    </View>
+                </ScrollView>
             </View>
         </>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    scrollViewContent: {
+        backgroundColor: 'white',
+        paddingTop: 20,
+        flexGrow: 1,
+    },
+    inner: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        margin: 10,
+        paddingHorizontal: 10,
+    },
+});
 
 export default RegisterPatient;
 
