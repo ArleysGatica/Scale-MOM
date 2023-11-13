e17c94765d4c26699e42328c29a19cd567c9d404
ArleysGatica <gatica.arleys16@gmail.com>
2023-11-12 22:28:20 -0600

    Update API base URL

---
+++ src/Helpers/Generalform/form/RenalForm.tsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import {ICardioVascular, IRenal} from '../../../types/types';
+import {View, StyleSheet, ScrollView} from 'react-native';
+import {Text, TextInput} from 'react-native-paper';
+
+interface IFields {
+  name: string;
+  label: string;
+  min?: number;
+  max?: number;
+  medida?: string;
+}
+
+interface Iprops {
+  fieldsRenal: Array<IFields>;
+  handleInputChangeRenal: (name: string, value: string) => void;
+  renalData: IRenal;
+}
+
+export const RenalForm = ({
+  fieldsRenal,
+  handleInputChangeRenal,
+  renalData,
+}: Iprops) => {
+  return (
+    <ScrollView
+      contentContainerStyle={styles.scrollViewContent}
+      keyboardShouldPersistTaps="handled">
+      <View style={styles.inner}>
+        <Text style={styles.header}>Renal</Text>
+      </View>
+      {fieldsRenal.map((field, index) => {
+        return (
+          <View
+            key={field.name}
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              width: '100%',
+              height: 80,
+              justifyContent: 'center',
+              flexWrap: 'wrap',
+              gap: 10,
+              columnGap: 10,
+              rowGap: 10,
+            }}>
+            <Text style={{width: 75, textAlign: 'center', fontSize: 15}}>
+              {field.name}
+            </Text>
+            <TextInput
+              style={{
+                width: 200,
+                height: 50,
+                marginBottom: 10,
+                backgroundColor: 'rgb(255, 255, 255)',
+                borderColor: 'rgba(0, 0, 0, 0.29)',
+                borderRadius: 4,
+                borderWidth: 1,
+                color: 'black',
+              }}
+              keyboardType="numeric"
+              onChangeText={text => handleInputChangeRenal(field.name, text)}
+              value={renalData[field.name as keyof IRenal]?.toString() || ''}
+              placeholder={`Min: ${field.min} Max: ${field.max}`}
+            />
+            <Text style={{width: 75, textAlign: 'center', fontSize: 15}}>
+              {field.medida}
+            </Text>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  header: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  input: {
+    width: '60%',
+    borderWidth: 1,
+    padding: 8,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10,
+  },
+  resultContainer: {
+    marginTop: 20,
+  },
+
+  scrollViewContent: {
+    backgroundColor: 'white',
+    paddingTop: 20,
+    flex: 1,
+    flexGrow: 1,
+    minHeight: 850,
+  },
+  inner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    maxHeight: 80,
+    marginBottom: 34,
+  },
+
+  containerDrop: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  picker: {
+    height: 50,
+    width: 200,
+    marginBottom: 20,
+  },
+  selectedText: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+});
---
+++ src/Helpers/Generalform/form/RespiratorioForm.tsx
@@ -0,0 +1,147 @@
+import React from 'react';
+import {ICardioVascular, IRespiratorio} from '../../../types/types';
+import {View, StyleSheet, ScrollView} from 'react-native';
+import {Text, TextInput} from 'react-native-paper';
+
+interface IFields {
+  name: string;
+  label: string;
+  min: number;
+  max: number;
+  medida: string;
+}
+
+interface Iprops {
+  fieldsRespiratorio: Array<IFields>;
+  handleInputChangeRespiratorio: (name: string, value: string) => void;
+  respiratorio: IRespiratorio;
+}
+
+export const RespiratorioForm = ({
+  fieldsRespiratorio,
+  handleInputChangeRespiratorio,
+  respiratorio,
+}: Iprops) => {
+  return (
+    <ScrollView
+      contentContainerStyle={styles.scrollViewContent}
+      keyboardShouldPersistTaps="handled">
+      <View style={styles.inner}>
+        <Text style={styles.header}>Respiratorio</Text>
+      </View>
+      {fieldsRespiratorio.map((field, index) => {
+        return (
+          <View
+            key={field.name}
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              width: '100%',
+              height: 80,
+              justifyContent: 'center',
+              flexWrap: 'wrap',
+              gap: 10,
+              columnGap: 10,
+              rowGap: 10,
+            }}>
+            <Text style={{width: 66, textAlign: 'center', fontSize: 15}}>
+              {field.name}
+            </Text>
+            <TextInput
+              style={{
+                width: 200,
+                height: 50,
+                marginBottom: 10,
+                backgroundColor: 'rgb(255, 255, 255)',
+                borderColor: 'rgba(0, 0, 0, 0.29)',
+                borderRadius: 4,
+                borderWidth: 1,
+                color: 'black',
+              }}
+              keyboardType="numeric"
+              onChangeText={text =>
+                handleInputChangeRespiratorio(field.name, text)
+              }
+              value={
+                respiratorio[field.name as keyof IRespiratorio]?.toString() ||
+                ''
+              }
+              placeholder={`Min: ${field.min} Max: ${field.max}`}
+            />
+            <Text style={{width: 75, textAlign: 'center', fontSize: 15}}>
+              {field.medida}
+            </Text>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  header: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  input: {
+    width: '60%',
+    borderWidth: 1,
+    padding: 8,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10,
+  },
+  resultContainer: {
+    marginTop: 20,
+  },
+
+  scrollViewContent: {
+    backgroundColor: 'white',
+    paddingTop: 20,
+    flex: 1,
+    flexGrow: 1,
+    maxHeight: 670,
+  },
+  inner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    maxHeight: 80,
+    marginBottom: 34,
+  },
+
+  containerDrop: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  picker: {
+    height: 50,
+    width: 200,
+    marginBottom: 20,
+  },
+  selectedText: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+});
