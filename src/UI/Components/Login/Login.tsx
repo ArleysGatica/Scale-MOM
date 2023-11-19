92980576a909ce8b2c867ed1275b1eeabf872c8c
arleygatica <gatica.arleys16@gmail.com>
2023-11-19 16:02:49 -0600

    Refactor Login component styles

--- src/UI/Components/Login/Login.tsx
+++ src/UI/Components/Login/Login.tsx
@@ -1,44 +1,39 @@
 import React, { useState } from 'react';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../../types/types';
-import { Text, View, TextInput, Image, Button, TouchableOpacity, SafeAreaView, StyleSheet, TouchableHighlight, Alert, ActivityIndicator, Pressable } from 'react-native';
+import { Text, View, TextInput, Image, Button, TouchableOpacity, SafeAreaView, StyleSheet, TouchableHighlight, Alert } from 'react-native';
 import style from './Style.Login';
 import { login } from '../../../services';
 
 type LoginFormProps = {
-  navigation?: NativeStackNavigationProp<
-    RootStackParamList,
-    | 'DoctorCreationForm'
-    | 'Login'
-    | 'CustomBottomTabNavigator'
-    | 'ListDoctor'
-    | 'ListPatient'
-  >;
+    navigation?: NativeStackNavigationProp<
+        RootStackParamList,
+        | 'DoctorCreationForm'
+        | 'Login'
+        | 'CustomBottomTabNavigator'
+        | 'ListDoctor'
+        | 'ListPatient'
+    >;
 };
 
 export function Login({ navigation }: LoginFormProps) {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleLogin = async () => {
-
-        const response = await login({username, password});
+        const response = await login({ username, password });
 
         if (response.userType === 1) {
             Alert.alert('Entrar como admin');
+            return navigation?.navigate('ListDoctor');
+        }
 
-            return navigation?.navigate('ListDoctor');     
-        }
-        
         if (response.userType === 2) {
-            navigation?.navigate('ListPatient');
             Alert.alert('Entrar como doctor');
-        } else {
-            Alert.alert('Credenciales incorrectas');
+            return navigation?.navigate('ListPatient');
         }
-        // console.log('Here');
-        
     };
 
     return (
@@ -49,17 +44,17 @@ export function Login({ navigation }: LoginFormProps) {
                 <Text style={style.txtSegund}>Login</Text>
             </View>
             <View style={{ width: '100%', alignItems: 'center' }}>
-
+                <Text style={style.txtForgot}>Nombre</Text>
                 <View style={style.passwordContainer}>
                     <TextInput
-                        testID="username-input" 
+                        testID="username-input"
                         style={style.passwordInput}
                         placeholderTextColor="gray"
                         placeholder="Nombre de usuario"
                         onChangeText={(text) => setUsername(text)}
                     />
                 </View>
-
+                <Text style={style.txtForgot}>Contraseña</Text>
                 <View style={style.passwordContainer} >
                     <TextInput
                         testID="password-input"
@@ -67,18 +62,30 @@ export function Login({ navigation }: LoginFormProps) {
                         placeholderTextColor="gray"
                         placeholder="Contraseña"
                         onChangeText={(text) => setPassword(text)}
-                        // secureTextEntry={true}
+                        secureTextEntry={!showPassword}
                     />
+                    <TouchableOpacity
+                        style={style.eyeIcon}
+                        onPress={() => setShowPassword(!showPassword)}
+                    >
+                        <Image
+                            source={showPassword
+                                ? require('../../../Assets/eye-open.png')
+                                : require('../../../Assets/eye-closed.png')
+                            }
+                            style={style.eyeImage}
+                        />
+                    </TouchableOpacity>
                 </View>
             </View>
-            <View style={{ width: '100%', alignItems: 'center', top:50 }}>
+            <View style={{ width: '100%', alignItems: 'center', top: 50 }}>
                 <TouchableHighlight onPress={() => handleLogin()} style={style.buttonSing}>
                     <Button
-                        testID="login-button" 
-                        color='rgb(18, 19, 48)'
+                        testID="login-button"
+                        color='#0B153C'
                         title='Iniciar Sesión'
                         onPress={() => handleLogin()}
-                   />
+                    />
                 </TouchableHighlight>
             </View>
         </SafeAreaView>
